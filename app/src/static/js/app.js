636e17c02a1f35b2757c7ae1675d867bb324cddc
Parvathy <116579961+106263@users.noreply.github.com>
2022-11-15 13:57:05 +0000

    Update app.js

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">hai parvathy!</p>
+                <p className="text-center">hai parvathy cloud computing!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
